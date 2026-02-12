69a90a648165b2b635460d60a1e0cd4269ac8580
Andrey Senkovich <senkovich@gmail.com>
2026-02-13 01:58:18 +0300

    Init commit

---
+++ src/main/java/ru/netology/javaqa/services/Radio.java
@@ -0,0 +1,64 @@
+package ru.netology.javaqa.services;
+
+public class Radio {
+    private int numberCurrentStation;
+    private int currentVolume;
+
+    public int getCurrentVolume() { //получение текущего уровня громкости
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int newCurrentVolume) { //выставление уровня громкости с проверкой разр. диапазона
+        if (newCurrentVolume < 0) {
+            return;
+        }
+        if (newCurrentVolume > 100) {
+            return;
+        }
+        currentVolume = newCurrentVolume;
+    }
+
+    public void increaseVolume() {  //увеличение уровня громкости на 1
+        if (currentVolume < 100) {
+            currentVolume++;
+        }
+    }
+
+    public void decreaseVolume() {  //уменьшение громкости на 1
+        if (currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+
+    public int getNumberCurrentStation() { //получение текущего номера станции
+        return numberCurrentStation;
+    }
+
+    public void setCurrentStation(int newNumberCurrentStation) { //выставление номера станции с проверкой разр. диапазона
+        if (newNumberCurrentStation < 0) {
+            return;
+        }
+        if (newNumberCurrentStation > 9) {
+            return;
+        }
+        numberCurrentStation = newNumberCurrentStation;
+    }
+
+    public void nextStation() {  //увеличение номера станции на 1
+        if (numberCurrentStation < 9) {
+            numberCurrentStation++;
+        }
+        if (numberCurrentStation == 9) {
+            numberCurrentStation = 0;
+        }
+    }
+
+    public void prevStation() {  //уменьшение номера станции на 1
+        if (numberCurrentStation > 0) {
+            numberCurrentStation--;
+        }
+        if (numberCurrentStation == 0) {
+            numberCurrentStation = 9;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/javaqa/services/RadioTest.java
@@ -0,0 +1,144 @@
+package ru.netology.javaqa.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    public void shouldSetNumberStation() { //проверка выставления заданного номера станции
+        Radio radio = new Radio();
+        radio.setCurrentStation(5);
+
+        int expected = 5;
+        int actual = radio.getNumberCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetNumberStationAboveRange() { //проверка выставления номера станции выше диапазона номеров
+        Radio radio = new Radio();
+        radio.setCurrentStation(10);
+
+        int expected = 0;
+        int actual = radio.getNumberCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSetNumberStationBelowRange() { //проверка выставления номера станции ниже диапазона номеров
+        Radio radio = new Radio();
+        radio.setCurrentStation(-1);
+
+        int expected = 0;
+        int actual = radio.getNumberCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNextNumberStation() { //проверка переключения на следующую станцию
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+
+        radio.nextStation();
+
+        int expected = 1;
+        int actual = radio.getNumberCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNextNumberStationOverMax() { //проверка переключения с последней станции на первую
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+
+        radio.nextStation();
+
+        int expected = 0;
+        int actual = radio.getNumberCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPrevNumberStation() { //проверка переключения на предыдущую станцию
+        Radio radio = new Radio();
+        radio.setCurrentStation(9);
+
+        radio.prevStation();
+
+        int expected = 8;
+        int actual = radio.getNumberCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPrevNumberStationDownMin() { //проверка переключения с первой станции на последнюю
+        Radio radio = new Radio();
+        radio.setCurrentStation(0);
+
+        radio.prevStation();
+
+        int expected = 9;
+        int actual = radio.getNumberCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaseVolume() { //проверка увеличения громкости на 1
+        Radio radio = new Radio();
+        radio.setCurrentVolume(50);
+
+        radio.increaseVolume();
+
+        int expected = 51;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldIncreaseVolumeOverMax() { //проверка увеличения громкости при достижение максимума
+        Radio radio = new Radio();
+        radio.setCurrentVolume(100);
+
+        radio.increaseVolume();
+
+        int expected = 100;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecreaseVolume() { //проверка уменьшения громкости на 1
+        Radio radio = new Radio();
+        radio.setCurrentVolume(50);
+
+        radio.decreaseVolume();
+
+        int expected = 49;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldDecreaseVolumeDownMin() { //проверка уменьшения громкости при достижение минимума
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+
+        radio.decreaseVolume();
+
+        int expected = 0;
+        int actual = radio.getCurrentVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
